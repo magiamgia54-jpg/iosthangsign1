35098063045662dfe81e745ece510935d305c02d
magiamgia54-jpg <magiamgia54@gmail.com>
2026-03-10 11:34:59 +0700

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,17 @@
+let apps = JSON.parse(localStorage.getItem("apps")) || [
+
+{
+name:"Delta VNG",
+desc:"Phiên bản mới 2026",
+icon:"https://i.imgur.com/7D7I6dI.jpeg",
+link:"itms-services://?action=download-manifest&url=https://magiamgia54-jpg.github.io/delta.plist"
+},
+
+{
+name:"Delta Fix Lag",
+desc:"Update mới nhất",
+icon:"https://i.imgur.com/7D7I6dI.jpeg",
+link:"itms-services://?action=download-manifest&url=https://magiamgia54-jpg.github.io/fixlag.plist"
+}
+
+]
